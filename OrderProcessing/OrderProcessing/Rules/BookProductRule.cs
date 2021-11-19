8ed72a11bd4f16404dc179aeba42c260da6ad22c
ST <swati@DESKTOP-UO6VNJ8>
2021-11-19 11:40:04 +0100

    Rules Skeleton classes

---
+++ OrderProcessing/OrderProcessing/Rules/BookProductRule.cs
@@ -0,0 +1,15 @@
+﻿using OrderProcessing.Common;
+using OrderProcessing.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace OrderProcessing.Rules
+{
+    public class BookProductRule : IPostPaymentRule
+    {
+        public bool ProcessingBizRules(Order order)
+        {
+            throw new NotImplementedException();
+        }
+     }
+}
