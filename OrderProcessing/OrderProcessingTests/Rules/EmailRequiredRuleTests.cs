cb5776f6c82a034c1b6c9fa006b9ac35b5cbbc65
ST <swati@DESKTOP-UO6VNJ8>
2021-11-19 12:54:35 +0100

    Email Required Rule and Sned Mail Processing Test cases

---
+++ OrderProcessing/OrderProcessingTests/Rules/EmailRequiredRuleTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderProcessing.Common;
+using OrderProcessing.Rules;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessing.Rules.Tests
+{
+    [TestClass()]
+    public class EmailRequiredRuleTests
+    {
+        [TestMethod()]
+        public void ProcessingBizRulesFailTest()
+        {
+            Order order = new Order()
+            {
+                CustomerDetail = new Customer { EmailId = "abc@gmail.com", Name = "TestName" },
+                ProductDetail = new Product { Catagories = ProductCatagories.Physical, Quantity = 2, UnitPrice = 50 },
+                AgentDetail = new Agent { AgentId = 1 }
+            };
+            Assert.AreNotEqual(order.EmailRequired(), order.SendEmail());
+        }
+        [TestMethod()]
+        public void ProcessingBizRulesSuccessTest()
+        {
+            Order order = new Order()
+            {
+                CustomerDetail = new Customer { EmailId = "abc@gmail.com", Name = "TestName" },
+                ProductDetail = new Product { Catagories = ProductCatagories.Membership, Quantity = 2, UnitPrice = 50 },
+                AgentDetail = new Agent { AgentId = 1 }
+            };
+            Assert.AreEqual(order.EmailRequired(), order.SendEmail());
+        }
+        [TestMethod()]
+        public void ProcessingBizRulesSuccessOnUpgradeTest()
+        {
+            Order order = new Order()
+            {
+                CustomerDetail = new Customer { EmailId = "abc@gmail.com", Name = "TestName" },
+                ProductDetail = new Product { Catagories = ProductCatagories.UpgradedMemberShip, Quantity = 2, UnitPrice = 50 },
+                AgentDetail = new Agent { AgentId = 1 }
+            };
+            Assert.AreEqual(order.EmailRequired(), order.SendEmail());
+        }
+    }
+}
